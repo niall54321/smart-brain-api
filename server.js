d925454a7c0c0e2b0be650faf336b2aada84729e
Niall Whelan <niallwhelan2010@hotmail.com>
2021-06-26 12:27:50 +0100

    Updating database URL

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: '127.0.0.1',
+		host: 'postgresqk-slippery-67950',
 		user: 'postgres',
 		password: 'chops1234',
 		database: 'smartbrain'
